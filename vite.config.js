7e1c2776ebb4d535bbeaa7a066839ed87b6a1f80
xhrbtk <xhtbtk@gmail.com>
2023-12-06 23:25:12 +0800

    更改base

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  assetsDir: 'assets',
+  base: '/my-record-demo/',
   plugins: [vue()],
 })
 
